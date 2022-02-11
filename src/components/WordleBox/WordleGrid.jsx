d276623bcc42fae484cc372f6617c43432c3e7a4
maarcoscuesta18 <marcosyjaime2@gmail.com>
2022-02-11 20:15:47 +0100

    adjust text

--- src/components/WordleBox/WordleGrid.jsx
+++ src/components/WordleBox/WordleGrid.jsx
@@ -4,11 +4,11 @@ export default function WordleGrid({guessLetters=[],maxTries,maxLength}) {
   return <div className="w-full grid grid-cols-5 gap-1">
       {Array.from(new Array(maxLength * maxTries), (_, idx) => {
           return <div key={idx} className={
-            "word-border font-bold text-xs h-14 rounded flex justify-center items-center  uppercase "+
+            "word-border font-bold rounded flex justify-center items-center  uppercase"+
           (guessLetters[idx]?.state === 'success' ? " bg-green-600" : '' )+
           (guessLetters[idx]?.state === 'warn' ? " bg-yellow-700" : '' )+
           (guessLetters[idx]?.state === 'error' ? " bg-zinc-800" : '' )
-        }><span className='px-3 sm:px-4'>{guessLetters[idx]?.letter}</span></div>
+        }>{guessLetters[idx]?.letter}</div>
         })}
   </div>;
 }
